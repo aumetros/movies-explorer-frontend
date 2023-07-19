bc4b3bf788b221ae3cc170c99bd61edc3cd22301
aumetros <aum3tros@yandex.ru>
2023-07-19 16:53:58 +0300

    Созданы компоненты Main и Header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Main from '../Main/Main';
 
 function App() {
   return (
     <div className="app">
-
-      <h1>Привет!</h1>
-      
+      <Main />      
     </div>
   );
 }
