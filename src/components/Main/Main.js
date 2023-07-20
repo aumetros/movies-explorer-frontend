6231f778ff3cb9109180e9b7693385fa0fd30d74
aumetros <aum3tros@yandex.ru>
2023-07-20 21:04:54 +0300

    Верстка компонента Techs - все разрешения

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,14 +3,16 @@ import Header from "../Header/Header";
 import Promo from "../Promo/Promo";
 import NavTab from "../NavTab/NavTab";
 import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs";
 
 function Main() {
   return (
     <div className="main">
       {/* <Header />
       <Promo />
-      <NavTab /> */}
-      <AboutProject />
+      <NavTab />
+      <AboutProject /> */}
+      <Techs />
     </div>
   );
 }
