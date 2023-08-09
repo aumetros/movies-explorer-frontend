fe987ba0e72c8c33893277397f915a16e949477b
aumetros <aum3tros@yandex.ru>
2023-08-09 18:37:13 +0300

    Обработка фильмов перенесена в компонент Movies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,85 +9,15 @@ import Profile from "../Profile/Profile";
 import Register from "../Register/Register";
 import Login from "../Login/Login";
 import NotFound from "../NotFound/NotFound";
-import { getMovies } from "../../utils/MoviesApi";
-import Modal from "../Modal/Modal";
 
 function App() {
-  const [movies, setMovies] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [isOpenModal, setIsOpenModal] = React.useState(false);
-  const [modalMessage, setModalMessage] = React.useState("");
-  const [isServerResponse, setIsServerResponse] = React.useState(true);
-  const [isMoviesFound, setIsMoviesFound] = React.useState(true);
-  const [filteredMovies, setFilteredMovies] = React.useState([]);
-
-  function handleNotFoundMessage(result) {
-    if (result.length === 0) {
-      setIsMoviesFound(false);
-    } else {
-      setIsMoviesFound(true);
-    }
-  }
-
-  function handleFilter(arr, request) {
-    const result = arr.filter((movie) => {
-      return (
-        movie.nameRU.toLowerCase().includes(request.toLowerCase()) ||
-        movie.nameEN.toLowerCase().includes(request.toLowerCase())
-      );
-    });
-    setFilteredMovies(result);
-    handleNotFoundMessage(result);
-  }
-
-  function handleFilterMovies(request) {
-    if (movies.length === 0) {
-      setIsLoading(true);
-      getMovies()
-        .then((movies) => {
-          setMovies(movies);
-          setIsServerResponse(true);
-          handleFilter(movies, request);
-        })
-        .catch((err) => {
-          console.log(err);
-          setIsServerResponse(false);
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
-    } else {
-      handleFilter(movies, request);
-    }
-  }
-
-  function handleOpenModal(msg) {
-    setModalMessage(msg);
-    setIsOpenModal(!isOpenModal);
-  }
-
-  function handleCloseModal() {
-    setIsOpenModal(false);
-  }
 
   return (
     <div className="app">
       <Layout>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route
-            path="/movies"
-            element={
-              <Movies
-                isLoading={isLoading}
-                movies={filteredMovies}
-                onError={handleOpenModal}
-                onErrorServer={isServerResponse}
-                onNotFound={isMoviesFound}
-                onSearch={handleFilterMovies}
-              />
-            }
-          />
+          <Route path="/movies" element={<Movies />} />
           <Route path="/saved-movies" element={<SavedMovies />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/signup" element={<Register />} />
@@ -95,11 +25,6 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
-      <Modal
-        isOpen={isOpenModal}
-        onClose={handleCloseModal}
-        modalMessage={modalMessage}
-      />
     </div>
   );
 }
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -4,51 +4,108 @@ import SearchForm from "../SearchForm/SearchForm";
 import Footer from "../Footer/Footer";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import React from "react";
+import { getMovies } from "../../utils/MoviesApi";
+import Modal from "../Modal/Modal";
 
-function Movies({
-  isLoading,
-  movies,
-  onError,
-  onErrorServer,
-  onNotFound,
-  onSearch,
-}) {
+function Movies() {
   const [isShortsChecked, setIsShortsCheked] = React.useState(true);
   const [renderMovies, setRenderMovies] = React.useState([]);
+  const [movies, setMovies] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isServerResponse, setIsServerResponse] = React.useState(true);
+  const [filteredMovies, setFilteredMovies] = React.useState([]);
+  const [isMoviesFound, setIsMoviesFound] = React.useState(true);
+  const [isOpenModal, setIsOpenModal] = React.useState(false);
+  const [modalMessage, setModalMessage] = React.useState("");
+
+  function handleSearchMovies(request) {
+    if (movies.length === 0) {
+      setIsLoading(true);
+      getMovies()
+        .then((movies) => {
+          setMovies(movies);
+          setIsServerResponse(true);
+          handleFilter(movies, request);
+        })
+        .catch((err) => {
+          console.log(err);
+          setIsServerResponse(false);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    } else {
+      handleFilter(movies, request);
+    }
+  }
+
+  function handleFilter(arr, request) {
+    const result = arr.filter((movie) => {
+      return (
+        movie.nameRU.toLowerCase().includes(request.toLowerCase()) ||
+        movie.nameEN.toLowerCase().includes(request.toLowerCase())
+      );
+    });
+    setFilteredMovies(result);
+    handleNotFoundMessage(result);
+  } 
+
+  function handleNotFoundMessage(result) {
+    if (result.length === 0) {
+      setIsMoviesFound(false);
+    } else {
+      setIsMoviesFound(true);
+    }
+  }
 
   function handleCheckShorts(status) {
     setIsShortsCheked(status);
-    console.log(status);
+  }
+
+  function handleOpenModal(msg) {
+    setModalMessage(msg);
+    setIsOpenModal(!isOpenModal);
+  }
+
+  function handleCloseModal() {
+    setIsOpenModal(false);
   }
 
   React.useEffect(() => {
     if (isShortsChecked) {
-      setRenderMovies(movies);
+      setRenderMovies(filteredMovies);
+      handleNotFoundMessage(filteredMovies);
     } else {
-      const moviesLongplay = movies.filter((movie) => {
+      const moviesLongplay = filteredMovies.filter((movie) => {
         return movie.duration < 60;
       });
       setRenderMovies(moviesLongplay);
+      handleNotFoundMessage(moviesLongplay);
     }
-  }, [movies, isShortsChecked]);
+  }, [filteredMovies, isShortsChecked]);
 
   return (
     <section className="movies">
       <Header />
       <main>
         <SearchForm
-          onSubmit={onSearch}
-          onError={onError}
+          onSubmit={handleSearchMovies}
+          onError={handleOpenModal}
           onShorts={handleCheckShorts}
         />
         <MoviesCardList
           movies={renderMovies}
           onLoading={isLoading}
-          onErrorServer={onErrorServer}
-          onNotFound={onNotFound}
+          onErrorServer={isServerResponse}
+          onNotFound={isMoviesFound}
         />
       </main>
       <Footer movies={true} />
+      <Modal
+        isOpen={isOpenModal}
+        onClose={handleCloseModal}
+        modalMessage={modalMessage}
+      />
     </section>
   );
 }
