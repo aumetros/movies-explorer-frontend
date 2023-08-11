f3883546ed976ac5cfb2acc2d25eea6e821b8932
aumetros <aum3tros@yandex.ru>
2023-08-11 14:48:38 +0300

    Изменен логотип модального окна

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -4,9 +4,7 @@ import SearchForm from "../SearchForm/SearchForm";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import MoviesCard from "../MoviesCard/MoviesCard";
 import Footer from "../Footer/Footer";
-import photo1 from "../../images/card-photo-1.png";
-import photo2 from "../../images/card-photo-2.png";
-import photo3 from "../../images/card-photo-3.png";
+
 
 function SavedMovies() {
   return (
@@ -15,9 +13,7 @@ function SavedMovies() {
       <main>
         <SearchForm />
         <MoviesCardList savedMovies={true}>
-          <MoviesCard photo={photo1} savedMovies={true} />
-          <MoviesCard photo={photo2} savedMovies={true} />
-          <MoviesCard photo={photo3} savedMovies={true} mob={true} />
+          
         </MoviesCardList>
       </main>
       <Footer />
